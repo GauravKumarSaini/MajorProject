9215fcc1e206f37114fe85363a5bbc2253c28935
Gaurav Kumar <gauravsaini2703@gmail.com>
2025-12-07 18:49:15 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,37 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        require:true,
+    },
+    description: String,
+    image:{
+        filename:{
+            type:String,
+            default:"listingimage",
+        },
+        url:{
+            type:String,
+            default:
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fphoto-image-art&psig=AOvVaw295x0R3XNdxDnvjdQ9bjA2&ust=1763752334018000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOjTzcy3gZEDFQAAAAAdAAAAABAE",
+            set:(v)=>
+                v===""
+            ?"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fphoto-image-art&psig=AOvVaw295x0R3XNdxDnvjdQ9bjA2&ust=1763752334018000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOjTzcy3gZEDFQAAAAAdAAAAABAE"
+            :v,
+        },     
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review",
+        },
+    ],
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
